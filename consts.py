37f97b2a697c7891ef09b36bb2ff37133d1e966f
jbt <dorinrassovsky2612@gmail.com>
2026-09-08 10:10:48 +0300

    adding consts

---
+++ consts.py
@@ -0,0 +1,22 @@
+START_MESSAGE = 'Welcome to The Flag game.\nHave Fun!'
+
+BOARD_ROWS = 25
+BOARD_COLS = 50
+CELL_SIZE = 20 # pixels per cell
+WINDOW_WIDTH = BOARD_COLS * CELL_SIZE
+WINDOW_HEIGHT = BOARD_ROWS * CELL_SIZE
+
+SOLDIER_ROWS = 4
+SOLDIER_COLS = 2
+SOLDIER_BODY_ROWS = 3 # the upper part
+SOLDIER_FEET_ROWS = 1 # the lower part
+
+FLAG_ROWS = 3
+FLAG_COLS = 4
+# game_field.py
+flag_row = BOARD_ROWS - FLAG_ROWS
+flag_col = BOARD_COLS - FLAG_COLS
+
+MINES_COUNT = 20
+MINE_ROWS = 1
+MINE_COLS = 3
